1d4a0ece5e5326771012467a4c5fd1de777a8f1e
Tim Remnant <tremnant@dizions.co.uk>
2019-04-23 14:17:35 +0100

    Adding the editor inputs

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -4,11 +4,24 @@
             <div class="row">
                 <div class="col-md-12 col-xs-12">
                     <label for="firstname">First Name</label>
-                    <input id="firstname" class="form-control" type="text"/>
+                    <input type="text" id="firstname" v-model="firstName" class="form-control"/>
                 </div>
                 <div class="col-md-12 col-xs-12">
                     <label for="lastname">Last Name</label>
-                    <input id="lastname" class="form-control" type="text"/>
+                    <input type="text" id="lastname" v-model="lastName" class="form-control"/>
+                </div>
+                <div class="col-md-12 col-xs-12">
+                    <label for="age">Age</label>
+                    <input type="number" id="age" v-model="age" class="form-control"/>
+                </div>
+                <div class="col-xs-12 col-md-12">
+                    <label for="country">Country</label>
+                    <select id="country" class="form-control" v-model="selectedCountry">
+                        <option :value="option.code" :key="option.code" v-for="option in countries">{{option.name}}</option>
+                    </select>
+                </div>
+                <div class="col-xs-12 col-md-12 p-3">
+                    <button class="btn btn-block btn-lg btn-primary" @click="createPerson()">Create Person</button>
                 </div>
             </div>
         </div>
@@ -16,7 +29,29 @@
 </template>
 
 <script>
+import Countries from '@/providers/Countries';
+
 export default {
-    name: "Editor"
+    name: "Editor",
+    data () {
+        return {
+            countries: [],
+            selectedCountry: 0,
+            firstName: '',
+            lastName: '',
+            age: 0
+        }
+    },
+    created () {
+        const that = this;
+        Countries.getAll().then((data) => {
+            that.countries = data;
+        });
+    },
+    methods: {
+        createPerson () {
+            
+        }
+    }
 }
 </script>
