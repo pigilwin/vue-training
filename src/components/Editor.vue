76564a58bbfdceaf030b526cef81f36b22877e7a
Tim Remnant <tremnant@dizions.co.uk>
2019-04-23 13:50:41 +0100

    Adding pages and beginning of components

---
+++ src/components/Editor.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-12 col-xs-12">
+                    <label for="firstname">First Name</label>
+                    <input id="firstname" class="form-control" type="text"/>
+                </div>
+                <div class="col-md-12 col-xs-12">
+                    <label for="lastname">Last Name</label>
+                    <input id="lastname" class="form-control" type="text"/>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Editor"
+}
+</script>
---
+++ src/views/Create.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="row mt-5">
+        <div class="col-md-12 col-xs-12">
+            <h2 class="text-center">Creating a new person</h2>
+        </div>
+        <div class="col-md-12 col-xs-12">
+            <person-editor></person-editor>
+        </div>
+    </div>
+</template>
+
+<script>
+import Editor from '@/components/Editor.vue'
+export default {
+    name: "Create",
+    components: {
+        "person-editor": Editor
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
