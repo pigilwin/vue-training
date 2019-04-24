43bda221b42832f23658c897ab06d4dab41baea7
Tim Remnant <tremnant@dizions.co.uk>
2019-04-24 13:30:40 +0100

    Changing route

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="nav justify-content-center">
         <router-link to="/" class="nav-link">Home</router-link>
-        <router-link to="/list" class="nav-link">List People</router-link>
+        <router-link to="/people" class="nav-link">List People</router-link>
         <router-link to="/create" class="nav-link">Create person</router-link>
         <router-link to="/settings" class="nav-link">Settings</router-link>
     </nav>
